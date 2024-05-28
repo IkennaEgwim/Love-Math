61d0db4bff41560071d2f5f3566f3d2399830f3d
Ikenna Egwim <egwimik@yahoo.com>
2024-05-28 04:09:41 +0000

    add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,18 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
 });
 
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
 function runGame() {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
@@ -36,7 +45,7 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
 }
 
